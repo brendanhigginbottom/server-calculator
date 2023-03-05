5426da23b7ac15efcfedff52e11eaedd700d76b5
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-03-05 10:49:32 -0600

    now capturing user operator selection as well as values

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,16 +1,44 @@
 console.log('JS Sourced');
 
+let selectedOperator = '';
+
 function submitForm(event) {
     event.preventDefault();
     console.log('In submitForm');
     let valueOne = Number(document.querySelector('#valueOne').value);
     let valueTwo = Number(document.querySelector('#valueTwo').value);
     console.log('Value 1:', valueOne, 'Value 2:', valueTwo);
+    let inputForServer = {
+        valueOne: valueOne,
+        valueTwo: valueTwo,
+        operator: selectedOperator
+    };
+    console.log(inputForServer);
 }
 
+function plusKey(event) {
+    selectedOperator = '+';
+    console.log(selectedOperator);
+}
 
+function minusKey(event) {
+    selectedOperator = '-';
+    console.log(selectedOperator);
+}
+
+function timesKey(event) {
+    selectedOperator = '*';
+    console.log(selectedOperator);
+}
+
+function divideKey(event) {
+    selectedOperator = '/';
+    console.log(selectedOperator);
+}
+
+// Clears input fields
 function clearFields(event) {
     console.log('In clear function');
     document.querySelector('#valueOne').value = '';
     document.querySelector('#valueTwo').value = '';
-};
+}
