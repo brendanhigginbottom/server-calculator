e08ced0cacd90404c4306baaa123dd2483e036eb
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-03-05 10:36:20 -0600

    Capturing inputs and added Clear button functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
 console.log('JS Sourced');
+
+function submitForm(event) {
+    event.preventDefault();
+    console.log('In submitForm');
+    let valueOne = Number(document.querySelector('#valueOne').value);
+    let valueTwo = Number(document.querySelector('#valueTwo').value);
+    console.log('Value 1:', valueOne, 'Value 2:', valueTwo);
+}
+
+
+function clearFields(event) {
+    console.log('In clear function');
+    document.querySelector('#valueOne').value = '';
+    document.querySelector('#valueTwo').value = '';
+};
